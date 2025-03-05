7e1b49915676b2e24da0eb3ae9ad295578677e1d
dungvip01xyz <168471127+dungvip01xyz@users.noreply.github.com>
2025-03-06 02:59:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const BOT_TOKEN = "8111648383:AAH0shy5bIro985TLQgGbca8zLouvOJB1T0"; // Thay bằ
 const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
 
 function sendMessage() {
-    const chatId = "8161946635";
+    const chatId = "8161946635"; // Thay bằng Chat ID bạn muốn gửi
     const message = document.getElementById("message").value;
     const status = document.getElementById("status");
+
     fetch(TELEGRAM_API, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
